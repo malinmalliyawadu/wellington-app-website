2e671a9526155b0177bcc2f168b0af9b46e2145e
Malin Malliya Wadu <malin.malliya.wadu@gmail.com>
2026-02-19 13:57:40 +1300

    feat: allow images from unsplash

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
         hostname: "octeacvgbszdwzluxaef.supabase.co",
         pathname: "/storage/v1/object/public/**",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
   },
 };
